692e60e2fc62a16fafd2ff8c3224acb25e54f395
sahadevan <sahadevan511@gmail.com>
2023-03-09 22:36:18 +0530

    add api's

--- Project/03_coffee_shop_full_stack/starter_code/backend/src/api.py
+++ Project/03_coffee_shop_full_stack/starter_code/backend/src/api.py
@@ -11,13 +11,23 @@ app = Flask(__name__)
 setup_db(app)
 CORS(app)
 
+# base_url = https://dev-gz5tjefp2qw5ta7u.us.auth0.com
+# options.Authority = "https://dev-gz5tjefp2qw5ta7u.us.auth0.com/";
+# options.Audience = "drinks";
+# barista@gmail.com, barista@123
+# manager@gmail.com, manager@123
+# https://dev-gz5tjefp2qw5ta7u.us.auth0.com/authorize?audience=drinks&response_type=token&client_id=J5za50yzGym3IF6Ayv1GBy7sIb88qMnK&redirect_uri=https://127.0.0.1:5000/login-results
+
+#vaishnavi-manager-access_token=eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6InlLMGxKS1UtN0xjcndvT0RTbEUxdCJ9.eyJpc3MiOiJodHRwczovL2Rldi1nejV0amVmcDJxdzV0YTd1LnVzLmF1dGgwLmNvbS8iLCJzdWIiOiJnb29nbGUtb2F1dGgyfDEwODI2MTk1NjE2MTk3MzY0MDM0MSIsImF1ZCI6ImRyaW5rcyIsImlhdCI6MTY3ODEyMTkwOSwiZXhwIjoxNjc4MTI5MTA5LCJhenAiOiJKNXphNTB5ekd5bTNJRjZBeXYxR0J5N3NJYjg4cU1uSyIsInNjb3BlIjoiIiwicGVybWlzc2lvbnMiOlsiZGVsZXRlOmRyaW5rcyIsImdldDpkcmlua3MiLCJnZXQ6ZHJpbmtzLWRldGFpbCIsInBhdGNoOmRyaW5rcyIsInBvc3Q6ZHJpbmtzIl19.DDcp3-aP6Ee5AFDcBerzkwtTplLjEuGSpSvlMa-1B9AXP475zCtBDcMfWTDZCeQZfJKHI6F7jOsZ7N6UeTiaYB9AapgY2oI0_hRE0KL5ZW92Sh28R7D5k1wb6_Qx7xgXL8xbsbdaAjM6LZriJz0GNDJv-rJHwYjgeSSn-JSkig0OXFWk728SJde64MAY-by-gzmF9clhJ_1TCbfVCPjKf8S3vIlK1FzC-6y-gKWJ7jBstsCBeCMSO0Ok6bOSBPqzkFR_ZryzwI6_FhYDylpRaf5KskC_eN9KPyPNSSGvECp1PQkgBsN8iWkigeHKIcsm8aY8VCFvUdtyS7bpqJyxPw
+
 '''
 @TODO uncomment the following line to initialize the datbase
 !! NOTE THIS WILL DROP ALL RECORDS AND START YOUR DB FROM SCRATCH
 !! NOTE THIS MUST BE UNCOMMENTED ON FIRST RUN
 !! Running this funciton will add one
 '''
-# db_drop_and_create_all()
+with app.app_context():
+    db_drop_and_create_all()
 
 # ROUTES
 '''
@@ -28,6 +38,13 @@ CORS(app)
     returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
         or appropriate status code indicating reason for failure
 '''
+@app.route('/drinks', methods=['GET'])
+def get_drinks():
+    drinks = Drink.query.all()
+    if len(drinks) == 0: 
+        abort(404)       
+    formatted_drinks = [drink.short() for drink in drinks]       
+    return jsonify({ 'success': True, 'drinks': formatted_drinks })
 
 
 '''
@@ -38,7 +55,13 @@ CORS(app)
     returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
         or appropriate status code indicating reason for failure
 '''
-
+@app.route('/drinks-detail', methods=['GET'])
+def get_drinks_detail():
+    drinks = Drink.query.all()
+    if len(drinks) == 0: 
+        abort(404)       
+    formatted_drinks = [drink.long() for drink in drinks]       
+    return jsonify({ 'success': True, 'drinks': formatted_drinks })
 
 '''
 @TODO implement endpoint
@@ -49,7 +72,17 @@ CORS(app)
     returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the newly created drink
         or appropriate status code indicating reason for failure
 '''
-
+@app.route('/drinks', methods=['POST'])
+def create_drinks():
+    body = request.get_json()
+    try:
+         title = body.get('title', None)
+         receipe = body.get('recipe', None)
+         drink = Drink(title = title, receipe = receipe)         
+         filtered_drink = Drink.query.filter(Drink.id == drink.id).one_or_none()     
+         return jsonify({ 'success': True, 'drinks': None if filtered_drink is None else filtered_drink.long() })
+    except:
+         abort(422)
 
 '''
 @TODO implement endpoint
@@ -83,23 +116,20 @@ Example error handling for unprocessable entity
 
 @app.errorhandler(422)
 def unprocessable(error):
-    return jsonify({
-        "success": False,
-        "error": 422,
-        "message": "unprocessable"
-    }), 422
+    return jsonify({ "success": False, "error": 422, "message": "unprocessable" }), 422
 
+@app.errorhandler(400)
+def bad_request(error):
+        return jsonify({ "success": False,  "error": 400, "message": "bad request" }), 400
 
-'''
-@TODO implement error handlers using the @app.errorhandler(error) decorator
-    each error handler should return (with approprate messages):
-             jsonify({
-                    "success": False,
-                    "error": 404,
-                    "message": "resource not found"
-                    }), 404
+@app.errorhandler(405)
+def method_not_allowed(error):
+        return jsonify({ "success": False, "error": 405, "message": "method not allowed" }), 405
 
-'''
+@app.errorhandler(500)
+def internal_error(error):
+        return jsonify({ "success": False, "error": 500, "message": "internal server error" }), 500
+
 
 '''
 @TODO implement error handler for 404
--- Project/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
+++ Project/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
@@ -6,9 +6,9 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: '', // the auth0 domain prefix
-    audience: '', // the audience set for the auth0 app
-    clientId: '', // the client id generated for the auth0 app
+    url: 'dev-gz5tjefp2qw5ta7u.us.auth0.com', // the auth0 domain prefix
+    audience: 'drinks', // the audience set for the auth0 app
+    clientId: 'J5za50yzGym3IF6Ayv1GBy7sIb88qMnK', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
   }
 };
